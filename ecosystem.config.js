e5ddff5b00d8ef542c9087202e8a1fe6b523ff43
Kishore Padmanaban <kishorepadmanaban@gmail.com>
2019-08-20 18:51:18 +0530

    Stage url changed in env

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -7,12 +7,18 @@ module.exports = {
           env: {
               "PORT": 3040,
               "NODE_ENV": "development",
-              "DB_CONN" : "mongodb://akshay:qwerty@13.233.94.159/turftown",
+              "DB_CONN" : "mongodb://akshay:qwerty@13.233.5.103/turftown",
+              "PHP_SERVER": "http://159.65.146.12/app/turftown-backend"
+          },
+          env_stage: {
+              "PORT": 3040,
+              "NODE_ENV": "production",
+              "DB_CONN" : "mongodb://akshay:qwerty@13.233.5.103/turftown",
               "PHP_SERVER": "http://159.65.146.12/app/turftown-backend"
           },
           env_production: {
               "PORT": 3040,
-              "NODE_ENV": "production",
+              "NODE_ENV": "stage",
               "DB_CONN" : "mongodb://akshay:qwerty@13.233.94.159/turftown",
               "PHP_SERVER": "http://159.65.146.12/app/turftown-backend"
           }

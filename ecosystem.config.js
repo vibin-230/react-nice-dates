3687856e5891dcb745759273ae223a66470cd9bd
Kishore Padmanaban <kishorepadmanaban@gmail.com>
2019-08-22 09:24:47 +0530

    OTP issue fixed

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -8,19 +8,28 @@ module.exports = {
               "PORT": 3040,
               "NODE_ENV": "development",
               "DB_CONN" : "mongodb://akshay:qwerty@13.233.5.103/turftown",
-              "PHP_SERVER": "http://159.65.146.12/app/turftown-backend"
+              "PHP_SERVER": "http://159.65.146.12/app/turftown-backend",
+              "AWS_SECRET_KEY": "9SkVgIrzjl+PoiOZ5AVMDSHxkQzuS+qt4gYG8BS+",
+              "AWS_ACCESS_KEY": "AKIAJCWCKO7WP7A6PPYQ",
+              "AWS_REGION": "ap-south-1"
           },
           env_stage: {
               "PORT": 3040,
               "NODE_ENV": "production",
               "DB_CONN" : "mongodb://akshay:qwerty@13.233.5.103/turftown",
-              "PHP_SERVER": "http://159.65.146.12/app/turftown-backend"
+              "PHP_SERVER": "http://159.65.146.12/app/turftown-backend",
+              "AWS_SECRET_KEY": "9SkVgIrzjl+PoiOZ5AVMDSHxkQzuS+qt4gYG8BS+",
+              "AWS_ACCESS_KEY": "AKIAJCWCKO7WP7A6PPYQ",
+              "AWS_REGION": "ap-south-1"
           },
           env_production: {
               "PORT": 3040,
               "NODE_ENV": "stage",
               "DB_CONN" : "mongodb://akshay:qwerty@13.233.94.159/turftown",
-              "PHP_SERVER": "http://159.65.146.12/app/turftown-backend"
+              "PHP_SERVER": "http://159.65.146.12/app/turftown-backend",
+              "AWS_SECRET_KEY": "9SkVgIrzjl+PoiOZ5AVMDSHxkQzuS+qt4gYG8BS+",
+              "AWS_ACCESS_KEY": "AKIAJCWCKO7WP7A6PPYQ",
+              "AWS_REGION": "ap-south-1"
           }
         }
     ]
--- scripts/aws-s3.js
+++ scripts/aws-s3.js
@@ -12,20 +12,20 @@ const s3 = new aws.S3()
 
 const upload = (filename,folder,message,res) =>{
     var filePath = "./assets/"+filename;
-
+    console.log('test')
     //configuring parameters
     var params = {
     Bucket: 'turftown',
     Body : fs.createReadStream(filePath),
     Key : folder+"/"+Date.now()+"_"+path.basename(filePath)
     };
-
+    console.log('test')
     s3.upload(params, function (err, data) {
         //handle error
         if (err) {
           res.send({status:"failed", message: "failded to upload image to s3"})
         }
-      
+        console.log('test')
         //success
         if (data) {
             res.send({status:"success", message, imageurl:data.Location})

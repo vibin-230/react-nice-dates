91ce32ea989c01d621a52c311d7af21f16b5dc1f
Ubuntu <ubuntu@ip-172-31-32-196.ap-south-1.compute.internal>
2019-08-25 17:10:53 +0000

    Config changed

---
+++ deploy.sh
@@ -0,0 +1,5 @@
+cd ~/turftown-backend
+git pull
+npm install
+pm2 restart turftown
+pm2 logs turftown
--- ecosystem.config.js
+++ ecosystem.config.js
@@ -11,25 +11,31 @@ module.exports = {
               "PHP_SERVER": "http://159.65.146.12/app/turftown-backend",
               "AWS_SECRET_KEY": "9SkVgIrzjl+PoiOZ5AVMDSHxkQzuS+qt4gYG8BS+",
               "AWS_ACCESS_KEY": "AKIAJCWCKO7WP7A6PPYQ",
-              "AWS_REGION": "ap-south-1"
+              "AWS_REGION": "ap-south-1",
+              "DOMAIN": "http://localhost:3001/"
           },
           env_stage: {
               "PORT": 3040,
-              "NODE_ENV": "production",
+              "NODE_ENV": "stage",
               "DB_CONN" : "mongodb://akshay:qwerty@13.233.5.103/turftown",
               "PHP_SERVER": "http://159.65.146.12/app/turftown-backend",
               "AWS_SECRET_KEY": "9SkVgIrzjl+PoiOZ5AVMDSHxkQzuS+qt4gYG8BS+",
               "AWS_ACCESS_KEY": "AKIAJCWCKO7WP7A6PPYQ",
-              "AWS_REGION": "ap-south-1"
+              "AWS_REGION": "ap-south-1",
+              "DOMAIN":"http://ec2-13-233-5-103.ap-south-1.compute.amazonaws.com/",
+              "RAZORPAY_API": "rzp_test_xLRyYe3WX7insB:wtk7oizETOvj4qKeZS8nVSch"
+
           },
           env_production: {
               "PORT": 3040,
-              "NODE_ENV": "stage",
+              "NODE_ENV": "production",
               "DB_CONN" : "mongodb://akshay:qwerty@13.233.94.159/turftown",
               "PHP_SERVER": "http://159.65.146.12/app/turftown-backend",
               "AWS_SECRET_KEY": "9SkVgIrzjl+PoiOZ5AVMDSHxkQzuS+qt4gYG8BS+",
               "AWS_ACCESS_KEY": "AKIAJCWCKO7WP7A6PPYQ",
-              "AWS_REGION": "ap-south-1"
+              "AWS_REGION": "ap-south-1",
+              "DOMAIN":"https://turftown.in/",
+              "RAZORPAY_API": "rzp_test_xLRyYe3WX7insB:wtk7oizETOvj4qKeZS8nVSch"
           }
         }
     ]

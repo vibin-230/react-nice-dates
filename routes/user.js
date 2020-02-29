c561068c4b975f80a3dc2660f920a45d880d8377
swqat117 <akshay@turftown.in>
2020-02-29 15:55:50 +0530

    upcoming booking

--- routes/user.js
+++ routes/user.js
@@ -1797,6 +1797,9 @@ router.post('/upcoming_booking', verifyToken, (req, res, next) => {
           }
         })
         let event_booking_data = eventBooking.filter((key)=>{
+          console.log('bd',moment(key.booking_date).utc().format("YYYYMDDHHmm"))
+          console.log('bdr',moment().add(330,"minutes").format("YYYYMMDDHHmm"))
+
           if(Math.round(moment(key.booking_date).utc().format("YYYYMDDHHmm")) > Math.round(moment().add(330,"minutes").format("YYYYMMDDHHmm"))){
             return key
           }

6a8de52d849404ff5a20ba1e6d97469c573244e7
swqat117 <akshay@turftown.in>
2020-02-29 13:39:42 +0530

    upcoming booking

--- routes/user.js
+++ routes/user.js
@@ -1788,18 +1788,18 @@ router.post('/upcoming_booking', verifyToken, (req, res, next) => {
   Booking.find(filter).lean().populate('venue_data','venue').then(booking=>{
     EventBooking.find(eventFilter).lean().populate('event_id').then(eventBooking=>{
         result = Object.values(combineSlots(booking))
-        let booking_data =  result.length > 0 ? result.filter((key)=>{
+        let booking_data =result.filter((key)=>{
           if( moment(key.end_time).utc().format("YYYYMMDDHmm") > moment().format("YYYYMMDDHmm")){
             return key
           }
-        }) : []
-        let event_booking_data =  eventBooking.length > 0 ? eventBooking.map((key)=>{
+        })
+        let event_booking_data = eventBooking.filter((key)=>{
           if( moment(key.booking_date).utc().format("YYYYMMDDHmm") > moment().format("YYYYMMDDHmm")){
             return key
           }
-        }) : []
-        console.log(booking_data)
-        console.log('------',event_booking_data,eventBooking)
+        })
+        console.log("booking",booking_data)
+        console.log('------',event_booking_data)
         booking_data = [...booking_data,...event_booking_data]
         let finalResult = booking_data.sort((a, b) => moment(a.start_time).format("YYYYMMDDHHmm") > moment(b.start_time).format("YYYYMMDDHHmm") ? 1 : -1 )
         

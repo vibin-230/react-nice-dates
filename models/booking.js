fc26a982d93cbdf7801919ed15b91c0802daa1f2
kumar <kumariitb8@gmail.com>
2020-04-24 07:51:20 +0530

    fixes of alternate booking and invoice date group

--- models/booking.js
+++ models/booking.js
@@ -21,6 +21,10 @@ const schema = new Schema({
     type: Boolean,
     default: false
   },
+  alternate:{
+    type:Boolean,
+    default:false
+  },
   no_charge:Boolean,
   group_id:String,
   group_name:String,

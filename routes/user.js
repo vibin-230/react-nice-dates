1978dcf9ba2b494631a52a765b79a70940ee7324
swqat117 <akshay@turftown.in>
2020-05-01 02:03:46 +0530

    pass conversations and groups

--- routes/user.js
+++ routes/user.js
@@ -2031,6 +2031,7 @@ router.post('/bookings_and_games', verifyToken, (req, res, next) => {
   let past_date  = moment(req.body.todate).add(1,'month')
   let filter = {
     booking_status:{$in:["booked"]},
+    phone:req.phone
   }
   let cancel_filter = {
     booking_status:{$in:["cancelled"]},

e4f956e6510077fe246861bf82e0f95ca19c09f0
swqat117 <akshay@turftown.in>
2020-07-31 19:07:43 +0530

    pass

--- routes/feed.js
+++ routes/feed.js
@@ -25,7 +25,10 @@ const Conversation = require('../models/conversation');
 
 router.post('/shout_out/:id', verifyToken, (req, res, next) => {
     const filter = !req.body.status ? { $addToSet: { shout_out: { $each: [req.userId] } } ,$set:{shout_out_count:1} } :{ $pull: { shout_out:  req.userId  }}
-        Post.findByIdAndUpdate({_id: req.params.id},filter ).then(game=> {
+    User.findById({_id: req.userId},{}).lean().then(user=> {
+      let following = user.following
+          //following = following.concat(req.userId)   
+    Post.findByIdAndUpdate({_id: req.params.id},filter ).then(game=> {
                     Post.findOne({_id: req.params.id}).lean().populate('shout_out','_id name profile_picture phone handle name_status').populate('created_by','_id name profile_picture phone handle name_status').populate({ path: 'game', populate: [{ path: 'conversation' , populate :{path:'last_message'} },{path:'host',select:'_id name profile_picture phone handle name_status'},{path:'users',select:'_id name profile_picture phone handle name_status'},{path:'invites',select:'_id name profile_picture phone handle name_status'},{path:'venue',select:'venue'}] }).then((s)=>{
                         if( s && s.shout_out && s.shout_out.length>0 && s.shout_out.filter((a)=>a._id.toString() === req.userId.toString()).length > 0){
                             s['shout_out_status'] = true
@@ -33,7 +36,25 @@ router.post('/shout_out/:id', verifyToken, (req, res, next) => {
                         }else{
                             s['shout_out_status'] = false
                         }
+                        // var array3 = s && s.shout_out && s.shout_out.length>0 ? s.shout_out.filter(function(obj) { return following.indexOf(obj._id.toString()) !== -1; }):[]
+                        var array3 = s && s.shout_out && s.shout_out.length>0 ? s.shout_out.filter((obj)=> following.filter(a=>a.toString() === obj._id.toString()).length > 0  ):[]
+                        var string_array = array3.length > 0 ? array3.map((a)=>a.name_status ? a.name : a.handle):[]
+              let x = ''
+              if(string_array.length === 1){
+                x = `Shoutout by ${string_array[0]}`
+                 }else if(string_array.length === 2){
+                  x = `Shoutout by ${string_array[0]} and ${string_array[1]}`
+                 }
+                 else if(string_array.length === 3){
+                  x = `Shoutout by ${string_array[0]}, ${string_array[1]} and ${string_array[2]}`
+                }
+                else if(string_array.length >= 4){
+                  x = `Shoutout by ${string_array[0]}, ${string_array[1]}, ${string_array[2]} and ${string_array.length-3} more`
+                   
+                }
+                s['shout_line'] = x
                         res.send({status:"success", message:"shouted out fetched successfully", data: s})
+}).catch(next);
 }).catch(next);
 }).catch(next);
 
@@ -63,12 +84,13 @@ router.post('/get_town_games/', [
   ], (req, res, next) => {
     User.findById({_id: req.userId},{}).lean().then(user=> {
       let following = user.following
-          following = following.concat(req.userId)
+         // following = following.concat(req.userId)
+          console.log('following',following)
           const date = moment().add(5,'hours').add(30,'minutes')
           const date2 = moment(req.body.date).add(5,'hours').add(30,'minutes')
   
       const filter = req.body.sport === 'all' ? { created_by: { $in: following } ,town:true, host:{ $in: following },start_time:{$gte:date2}} :{ created_by: { $in: following } ,town:true,sport_name:{$in:req.body.sport}, host:{ $in: following }, start_time:{$gte:date}}
-      const filter1 = req.body.sport === 'all'? { created_by: { $in: following } ,status:true}:{ created_by: { $in: following } ,status:true,sport_name:req.body.sport}  
+      const filter1 = req.body.sport === 'all'? { $or:[{created_by: { $in: following } ,status:true},{shout_out: { $in: following } ,status:true}]}:{ created_by: { $in: following } ,status:true,sport_name:req.body.sport}  
       //Game.find(filter).lean().populate('conversation').populate('host','_id name profile_picture phone handle name_status').populate("venue","venue").populate('users','_id name profile_picture phone handle name_status').populate('invites','_id name profile_picture phone').then(existingConversation=>{
        Post.find(filter1).lean().populate('shout_out','_id name profile_picture phone handle name_status').populate('created_by','_id name profile_picture phone handle name_status').populate({ path: 'game', populate: [{ path: 'conversation' , populate :{path:'last_message'} },{path:'host',select:'_id name profile_picture phone handle name_status'},{path:'users',select:'_id name profile_picture phone handle name_status'},{path:'invites',select:'_id name profile_picture phone handle name_status'},{path:'venue',select:'venue'}] }).then((posts)=>{
         // existingConversation.map((key)=>{
@@ -82,19 +104,22 @@ router.post('/get_town_games/', [
               }else{
                 s['shout_out_status'] = false
               }
-              var array3 = s && s.shout_out && s.shout_out.length>0 ? s.shout_out.filter(function(obj) { return following.indexOf(obj._id.toString()) !== -1; }):[]
+              var array3 = s && s.shout_out && s.shout_out.length>0 ? s.shout_out.filter((obj)=> following.filter(a=>a.toString() === obj._id.toString()).length > 0  ):[]
+              //var array4 = s && s.shout_out && s.shout_out.length>0 ? s.shout_out.filter((obj)=> following.indexOf(obj._id.toString()) !== -1 ):[]
+
+             console.log('asdasdasdasdasdasd',array3,s.shout_out)
               var string_array = array3.length > 0 ? array3.map((a)=>a.name_status ? a.name : a.handle):[]
               let x = ''
               if(string_array.length === 1){
                 x = `Shoutout by ${string_array[0]}`
                  }else if(string_array.length === 2){
-                  x = `Shoutout by ${string_array[0]},${string_array[1]}`
+                  x = `Shoutout by ${string_array[0]} and ${string_array[1]}`
                  }
                  else if(string_array.length === 3){
-                  x = `Shoutout by ${string_array[0]},${string_array[1]},${string_array[2]}`
+                  x = `Shoutout by ${string_array[0]}, ${string_array[1]} and ${string_array[2]}`
                 }
                 else if(string_array.length >= 4){
-                  x = `Shoutout by ${string_array[0]},${string_array[1]},${string_array[2]} and ${string_array.length-3} more`
+                  x = `Shoutout by ${string_array[0]}, ${string_array[1]}, ${string_array[2]} and ${string_array.length-3} more`
                    
                 }
                 s['shout_line'] = x

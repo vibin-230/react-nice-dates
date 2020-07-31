23bb783ce268d90a1e33147bc075d690f8988c44
kumar <kumariitb8@gmail.com>
2020-07-31 22:11:10 +0530

    develop

--- chat/Chatroom.js
+++ chat/Chatroom.js
@@ -30,15 +30,15 @@ module.exports = function ({ _id, image }) {
 
   async function getChatHistory(id, token) {
     const user = await verifyToken(token)
-    const x = await Conversation.findById({ _id: id }).lean().then((conversation) => {
+    const x = await Conversation.findById({ _id: id }).populate("members","name profile_picture handle name_status").populate("host","name profile_picture handle name_status").lean().then((conversation) => {
       let date = conversation.join_date.length > 0 ? conversation.join_date.filter((jd) => jd.user_id.toString() === user.id.toString()) : []
-      const x = conversation.members.filter((a)=>a.toString() === user.id.toString())
+      const x = conversation.members.filter((a)=>a._id.toString() === user.id.toString())
       const user1 =   conversation.exit_list && conversation.exit_list.length > 0 && conversation.exit_list.filter((a)=> a && a.user_id && a.user_id._id.toString() === user.id.toString())
       console.log(user,x,user1,x.length);
       const filter  = x.length > 0 ?  date && date.length > 0 ? { conversation: id, created_at: { $gte: date[date.length-1].join_date } } : { conversation: id} :{ conversation: id, created_at: { $lte: moment(user1[user1.length-1].timeStamp).add(10,'seconds') } }
       conversation['exit'] = x.length > 0 ? false:true
       console.log(filter,conversation);
-      return Message.find(filter).lean().populate('author', 'name _id').populate('user', 'name _id profile_picture phone handle').populate({ path: 'game', populate: { path: 'conversation' , populate :{path:'last_message'} } }).sort({_id:-1}).limit(20).then(m => {
+      return Message.find(filter).lean().populate('author', 'name _id handle').populate('user', 'name _id profile_picture phone handle').populate({ path: 'game', populate: { path: 'conversation' , populate :{path:'last_message'} } }).sort({_id:-1}).limit(20).then(m => {
         return {messages:m.reverse(),conversation:conversation}
       }).catch((e) => console.log(e))
     }).catch((e) => console.log(e))

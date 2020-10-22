21544f67cebbdc8e75f1435cab81b59e0f93b957
kumar <kumariitb8@gmail.com>
2020-10-22 17:04:16 +0530

    fixes of chnages

--- routes/admin.js
+++ routes/admin.js
@@ -1490,7 +1490,7 @@ router.post('/search',
 	verifyToken,
 	AccessControl('venue', 'read'),
 	(req, res, next) => {
-	User.find({$and:[{ $or: [{"name":{ "$regex": req.body.search, "$options": "i" }}, {"handle":{ "$regex": req.body.search, "$options": "i" }}]}]},{__v:0,token:0,otp:0,activity_log:0}).then(user=>{
+	User.find({$and:[{ $or: [{"name":{ "$regex": req.body.search, "$options": "i" }}, {"handle":{ "$regex": req.body.search, "$options": "i"}}]},{'handle':{$exists:true,$ne:null }} ]},{__v:0,token:0,otp:0,activity_log:0}).then(user=>{
 		Venue.find({"venue.name":{ "$regex": req.body.search, "$options": "i" }}).then(venue=>{
 		Event.find({"event.name":{ "$regex": req.body.search, "$options": "i" },"start_date":{$gte:new Date()}}).lean().populate('venue').then(event=>{
 			Offers.find({}).then(offers=>{

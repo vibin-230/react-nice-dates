1ae396e47974f1de506c0a725b4e88013e9c9aa7
swqat117 <akshay@turftown.in>
2020-10-27 17:24:56 +0530

    added timestamp

--- chat/Chatroom.js
+++ chat/Chatroom.js
@@ -59,6 +59,7 @@ module.exports = function ({ _id, image }) {
       conversation['exit'] = x.length > 0 ? false:true
       conversation['exit2'] = x.length > 0 ? false:true
       return Message.find(filter).lean().populate('author', 'name _id handle').populate('user', 'name _id profile_picture phone handle visibility').populate({path:"event",populate:{path:"venue",select:"venue"}}).populate({ path: 'game', populate: { path: 'conversation' , populate :{path:'last_message'} } }).sort({_id:-1}).limit(20).then(m => {
+        m.splice(0,0,{conversation:conversation._id,message:parseDate(moment(m[0].created_at).utc().format('MM-DD-YYYY')),name:'bot',read_status:false,read_by:user.id,author:user.id,type:'bot',created_at:m[0].created_at,time:true})
         for(let i = 1 ;i <m.length; i++){
           if( moment(m[i].created_at).utc().format('MM-DD-YYYY') !== moment(m[i-1].created_at).utc().format('MM-DD-YYYY')){
               m.splice(i,0,{conversation:conversation._id,message:parseDate(moment(m[i-1].created_at).utc().format('MM-DD-YYYY')),name:'bot',read_status:false,read_by:user.id,author:user.id,type:'bot',created_at:m[i].created_at,time:true})

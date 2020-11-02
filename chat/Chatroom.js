4b7e55fc57a61441974cfc8152c48792df48c43c
swqat117 <akshay@turftown.in>
2020-11-02 19:11:46 +0530

    added venue in populate in getChatHistory

--- chat/Chatroom.js
+++ chat/Chatroom.js
@@ -58,7 +58,7 @@ module.exports = function ({ _id, image }) {
       const filter  = x.length > 0 ?  date && date.length > 0 ? { conversation: id, created_at: { $gte: date[date.length-1].join_date } } : { conversation: id} :{ conversation: id, created_at: { $lte: moment(user1[user1.length-1].timeStamp).add(10,'seconds') } }
       conversation['exit'] = x.length > 0 ? false:true
       conversation['exit2'] = x.length > 0 ? false:true
-      return Message.find(filter).lean().populate('author', 'name _id handle').populate('user', 'name _id profile_picture phone handle visibility').populate({path:"event",populate:{path:"venue",select:"venue"}}).populate({ path: 'game', populate: { path: 'conversation' , populate :{path:'last_message'} } }).sort({_id:-1}).limit(20).then(m => {
+      return Message.find(filter).lean().populate('author', 'name _id handle').populate('user', 'name _id profile_picture phone handle visibility').populate({path:"event",populate:{path:"venue",select:"venue"}}).populate({ path: 'game', populate:[ { path: 'conversation' , populate :{path:'last_message'} },{path:'venue', model:'venue',select:'venue'}] }).sort({_id:-1}).limit(20).then(m => {
         for(let i = 1 ;i <m.length; i++){
           if( moment(m[i].created_at).utc().format('MM-DD-YYYY') !== moment(m[i-1].created_at).utc().format('MM-DD-YYYY')){
             m.splice(i,0,{conversation:conversation._id,message:parseDate(moment(m[i-1].created_at).utc().format('MM-DD-YYYY')),name:'bot',read_status:false,read_by:user.id,author:user.id,type:'bot',created_at:m[i].created_at,time:true})

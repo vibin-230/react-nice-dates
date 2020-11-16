aa618b143e48ff117833ac3ed25752c95645bdcd
swqat117 <akshay@turftown.in>
2020-11-16 14:45:20 +0530

    passhandlers

--- chat/handlers.js
+++ chat/handlers.js
@@ -321,12 +321,12 @@ async function handleUpdateGroup({ chatroomName, message,members,colors } = {}, 
 
   async function handleSendBroadcast(message,callback){
     
-    const clientNumber = io.sockets.adapter.rooms[message.conversation];
-    const activeUsers = clientManager.filterClients(Object.keys(clientNumber.sockets))
+    //const clientNumber = io.sockets.adapter.rooms[message.conversation];
+    //const activeUsers = clientManager.filterClients(Object.keys(clientNumber.sockets))
        io.in(message.conversation).emit('new',message)
         client.to(message.conversation).emit('unread',message)
         chatroomManager.saveMessage(message) 
-       // chatroomManager.notifyAllUsersNotInTheChatroom(chatroomName, message,activeUsers)
+        chatroomManager.notifyAllUsersNotInTheChatroom(chatroomName, message,[])
         callback()
   }
 

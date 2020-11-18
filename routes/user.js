a530b1671b132422bbf5c2a02e22e521d16f7646
kumar <kumariitb8@gmail.com>
2020-11-18 17:41:04 +0530

    FIXEs of mvp text changfes

--- routes/user.js
+++ routes/user.js
@@ -3272,17 +3272,17 @@ router.post('/modify_booking/:id', verifyToken, (req, res, next) => {
         let end_time = Object.values(booking).reduce((total,value)=>{return total>value.end_time?total:value.end_time},booking[0].end_time)
         let datetime = date + " " + moment(start_time).format("hh:mma") + "-" + moment(end_time).format("hh:mma")
         //Activity Log
-        let activity_log = {
-          datetime: new Date(),
-          id:req.userId,
-          user_type: req.role?req.role:"user",
-          activity: 'slot modified',
-          name:req.name,
-          booking_id:booking_id,
-          venue_id:booking[0].venue_id,
-          message: "Slot "+booking_id+" modified at "+venue_name+" "+datetime+" "+venue_type,
-        }
-        ActivityLog(activity_log)
+        // let activity_log = {
+        //   datetime: new Date(),
+        //   id:req.userId,
+        //   user_type: req.role?req.role:"user",
+        //   activity: 'slot modified',
+        //   name:req.name,
+        //   booking_id:booking_id,
+        //   venue_id:booking[0].venue_id,
+        //   message: "Slot "+booking_id+" modified at "+venue_name+" "+datetime+" "+venue_type,
+        // }
+        // ActivityLog(activity_log)
       })
     })
   })
@@ -3300,16 +3300,19 @@ router.post('/booking_completed/:id', verifyToken, (req, res, next) => {
         Game.findOne({"bookings.booking_id":booking[0].booking_id}).then((g)=>{
         if(g){
           Game.findOneAndUpdate({"bookings.booking_id":booking[0].booking_id},{$set:{bookings:booking,completed:true,booking_status:"completed"}}).populate("users","name handle device_token").then((a)=>{
-            Message.create({conversation:a.conversation,message:`Game completed! please pick an MVP for this game.`,name:'bot',read_status:true,read_by:a.host[0],author:a.host[0],type:'bot',created_at:new Date()}).then(message1=>{
+           let final_Game = (game.sport_name == "cricket" || game.sport_name == "badminton") ? (game.users.length >= 3 ? true : false) : (game.sport_name == "football" || game.sport_name == "basketball") ? (game.users.length >=4 ? true :false) : false
+           if(final_Game){ 
+           Message.create({conversation:a.conversation,message:`Game completed! please pick an MVP for this game.`,name:'bot',read_status:true,read_by:a.host[0],author:a.host[0],type:'bot',created_at:new Date()}).then(message1=>{
                Conversation.findByIdAndUpdate({_id:a.conversation},{$set:{last_message:message1._id, last_updated:new Date()}}).then((m)=>{ 
                 Conversation.findById({_id:a.conversation}).then((m)=>{                      
                 const device_token_list=a.users.map((e)=>e.device_token)
-                NotifyArray(device_token_list,`Game completed! please pick an MVP`,'Turf Town',m)
+                NotifyArray(device_token_list,`Game completed! Please pick an MVP for ${a.name}`,'Turf Town',m)
                   return user.map((e)=>e._id)
                 }).catch(next);
                }).catch(next);
               }).catch(next);
-            }).catch(next);
+        }
+        }).catch(next);
         }
         result = Object.values(combineSlots(booking))
         res.send({status:"success", message:"booking completed", data:result})
@@ -3321,23 +3324,23 @@ router.post('/booking_completed/:id', verifyToken, (req, res, next) => {
         let start_time = Object.values(booking).reduce((total,value)=>{return total<value.start_time?total:value.start_time},booking[0].start_time)
         let end_time = Object.values(booking).reduce((total,value)=>{return total>value.end_time?total:value.end_time},booking[0].end_time)
         let datetime = date + " " + moment(start_time).format("hh:mma") + "-" + moment(end_time).format("hh:mma")
-        //Activity Log
-        let activity_log = {
-          datetime: new Date(),
-          id:req.userId,
-          user_type: req.role?req.role:"user",
-          activity: 'slot booking completed',
-          name:req.name,
-          venue_id:booking[0].venue_id,
-          booking_id:booking_id,
-          message: "Slot "+booking_id+" booking completed at "+venue_name+" "+datetime+" "+venue_type,
-        }
-        ActivityLog(activity_log)
-      })
-    })
-  })
+        // //Activity Log
+        // let activity_log = {
+        //   datetime: new Date(),
+        //   id:req.userId,
+        //   user_type: req.role?req.role:"user",
+        //   activity: 'slot booking completed',
+        //   name:req.name,
+        //   venue_id:booking[0].venue_id,
+        //   booking_id:booking_id,
+        //   message: "Slot "+booking_id+" booking completed at "+venue_name+" "+datetime+" "+venue_type,
+        // }
+        // ActivityLog(activity_log)
+      }).catch(next)
+    }).catch(next)
+  }).catch(next)
 
-  })
+  }).catch(next)
 })
 
 

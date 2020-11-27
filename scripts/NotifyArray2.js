44e727694c3de7a749d2e9c69d3701637f31314c
Kumar <kumar@Kumars-MacBook-Pro.local>
2020-11-27 19:32:20 +0530

    fixes of notification click

--- scripts/NotifyArray2.js
+++ scripts/NotifyArray2.js
@@ -1,18 +1,8 @@
 const gcm = require('node-gcm');
 const sender = new gcm.Sender('AAAAa6sWgd8:APA91bGNw2CJaOhW6EjEUB1Vw88ahctBpG2zn00l7oPFYB21GP5VUiMxEtA0QtPepmhf6S65dpcSP4SIN9cntzXjmhqzRwjGo7ASQ-S2K1LVC7Q2kB1eNAv08iJZRg4jK2e_IdqX4xDk');
 
-function NotifyArray(device_token, message,title,chatroom1) {
+function NotifyArray(device_token, message,title,chatroom) {
   //console.log(chatroom)
-    let chatroom = Object.assign({},chatroom1)
-      if(chatroom && chatroom._id){
-        chatroom['exit_list'] = []
-        chatroom['join_date'] = []
-        chatroom['last_active'] = []
-        chatroom['last_message'] = {}
-      }
-      else {
-        chatroom = {}
-      }
     const title1 = title.toString.length > 0 ? title : 'Turftown'
     var message = new gcm.Message({
         collapseKey: 'demo',
